a14498db1b34ea93167e2d8527d35069e1f3f694
Vishal Jagtap <89152210+vishalj1412@users.noreply.github.com>
2021-11-02 15:44:27 +0530

    Added: search the contact in addressbook

---
+++ Contacts.java
@@ -0,0 +1,88 @@
+package com.addressbook.streamapi;
+
+class Contacts {
+    private String firstName, lastName, address, city, state, email;
+    private long zip;
+    private long phoneNumber;
+
+    public Contacts (String firstName, String lastName, String address, String city, String state, Long zip,
+                     Long phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String toString() {
+        return ("First name: " + firstName + " Last name: " + lastName + " Address: " + address + " city: " + city
+                + " state: " + state + " email: " + email + " zip: " + zip + " phone number:" + phoneNumber + "");
+    }
+}
